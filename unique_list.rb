cdca746a3b1ff871505de0cacacff488c51978a6
Clint Osterholz <clintosterholz@gmail.com>
2016-04-10 19:35:20 -0400

    adds attr_accessor and notes

--- unique_list.rb
+++ unique_list.rb
@@ -1,36 +1,37 @@
 class UniqueList
 
+# creates numbers variable as an empty hash
   def initialize
-    @number = {}
+    @numbers = {}
   end
 
+# method that allows variable to read hash keys
   def values
-    @number.keys
+    @numbers.keys
   end
 
+# method that creates key value pairs that are passed in through spec
   def add(int)
-    @number[int] = int
+    @numbers[int] = int
   end
 
+# method that deletes hashes
   def remove(int)
-    @number.delete(int)
+    @numbers.delete(int)
   end
 
+# method to perform searches based on an int thats passed in
   def find(int)
     @node = Node.new
-    @node.value = @number[int]
+    @node.value = @numbers[int]
     if !@node.value.nil?
       @node
     end
   end
 
+# class wrapper and accessor to create instance variable .value
   class Node
-    def value=(int)
-      @variable = int
-    end
-    def value
-      @variable
-    end
+    attr_accessor :value
   end
 
 end
