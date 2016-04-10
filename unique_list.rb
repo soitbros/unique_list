f98507ccddcaac2f758aaf3cf601160858e1e42a
Clint Osterholz <clintosterholz@gmail.com>
2016-04-10 02:58:49 -0400

    adds I think working tests??

--- unique_list.rb
+++ unique_list.rb
@@ -1,2 +1,36 @@
 class UniqueList
+
+  def initialize
+    @number = {}
+  end
+
+  def values
+    @number.keys
+  end
+
+  def add(int)
+    @number[int] = int
+  end
+
+  def remove(int)
+    @number.delete(int)
+  end
+
+  def find(int)
+    @node = Node.new
+    @node.value = @number[int]
+    if !@node.value.nil?
+      @node
+    end
+  end
+
+  class Node
+    def value=(int)
+      @variable = int
+    end
+    def value
+      @variable
+    end
+  end
+
 end
